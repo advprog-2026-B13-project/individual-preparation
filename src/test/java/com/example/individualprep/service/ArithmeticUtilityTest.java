2a2668f3c179570580d3bc5869ef4d36e5bd1a07
Mednoob <ahmadyaqdhan123@gmail.com>
2026-02-13 18:01:51 +0700

    test: add unit tests for ArithmeticUtility divide method

--- src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -1,8 +1,9 @@
 package com.example.individualprep.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ArithmeticUtilityTest {
 
@@ -25,5 +26,47 @@ class ArithmeticUtilityTest {
     @Test
     void exponentThrowsWhenExponentIsNegative() {
         assertThrows(IllegalArgumentException.class, () -> arithmeticUtility.exponent(2.0, -1));
+    }
+
+    @Test
+    public void testDivide_positiveInputs() {
+        double toleranceDelta = 0.0001;
+
+        double num = 2.5;
+        double divisor = 5;
+
+        assertEquals(0.5, arithmeticUtility.divide(num, divisor), toleranceDelta);
+    }
+
+    @Test
+    public void testDivide_negativeInputs() {
+        double toleranceDelta = 0.0001;
+
+        double num = -1.2;
+        double divisor = 2;
+        assertEquals(-0.6, arithmeticUtility.divide(num, divisor), toleranceDelta);
+
+        num = 0.6;
+        divisor = -3;
+        assertEquals(-0.2, arithmeticUtility.divide(num, divisor), toleranceDelta);
+
+        num = -0.5;
+        divisor = -0.5;
+        assertEquals(1, arithmeticUtility.divide(num, divisor), toleranceDelta);
+    }
+
+    @Test
+    public void testDivide_zeroInputs() {
+        double num1 = 10;
+        double divisor1 = 0;
+        assertThrows(IllegalArgumentException.class, () -> arithmeticUtility.divide(num1, divisor1));
+
+        double num2 = 0;
+        double divisor2 = 1.1;
+        assertEquals(0.0, arithmeticUtility.divide(num2, divisor2));
+
+        double num3 = 0;
+        double divisor3 = 0;
+        assertThrows(IllegalArgumentException.class, () -> arithmeticUtility.divide(num3, divisor3));
     }
 }

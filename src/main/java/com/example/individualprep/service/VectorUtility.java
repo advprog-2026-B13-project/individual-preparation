b3f81f088713f2d69eb65c2b1d5dfbfd7ae61740
qowiys <qowiyshabir1416@gmail.com>
2026-02-13 18:16:28 +0700

    fix merge error

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -44,20 +44,7 @@ public class VectorUtility {
 
     public double dotProduct(double[] v1, double[] v2) {
         // TODO: Implement me properly!
-        if (v1 == null || v2 == null) {
-            throw new IllegalArgumentException("Vectors tidak boleh null");
-        }
-
-        if (v1.length != v2.length) {
-            throw new IllegalArgumentException("Panjang vektor harus sama");
-        }
-
-        double result = 0.0;
-
-        for (int i = 0; i < v1.length; i++) {
-            result += v1[i] * v2[i];
-        }
-        return result;
+        return 0.0;
     }
 
     public double norm(double[] v1) {
